ab66a1d4e2ba0da410e9b9b8029cf9276338961b
FrancoBujakiewicz <franconatahelbuja@gmail.com>
2025-12-31 17:29:10 -0300

    fix: Removing keymap help buffer using that keybind.

--- config/keybindings/prompts.el
+++ config/keybindings/prompts.el
@@ -2,10 +2,11 @@
  (global-set-key (kbd "C-f") (lambda ()
  (interactive) (find-alternate-file (read-file-name ""))))
 
- (global-set-key (kbd "C-h k") 'describe-key)
  (global-set-key (kbd "C-h s") 'shell-command)
  (global-set-key (kbd "C-h e") 'eval-expression)
- (global-set-key (kbd "C-h r") 're-builder)
-
  (global-set-key (kbd "C-h h") (lambda ()
  (interactive) (prin1 (text-properties-at (point)))))
+
+ ;; Keymap help mini-buffer when that binding. I removed it.
+
+ (global-set-key (kbd "C-h C-h") 'ignore)
