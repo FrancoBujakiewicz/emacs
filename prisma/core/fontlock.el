867840f0dda2d29e2ae1eb3761602edeccb552f3
FrancoBujakiewicz <franconatahelbuja@gmail.com>
2026-01-01 17:35:38 -0300

    fix: Matching numbers as last character in fallback face.

--- prisma/core/fontlock.el
+++ prisma/core/fontlock.el
@@ -3,17 +3,16 @@
 
  (dolist (mode '(emacs-lisp-mode lisp-mode scheme-mode))
 
- (font-lock-add-keywords mode 
+ (font-lock-add-keywords mode
 
   `(("\\<\\(nil\\|t\\)\\>" 1 'lisp-builtin-values-face prepend)
 
-   ("\\([a-z][a-z-]*[0-9]*\\)" 1 
+   ("\\([a-z][-a-z]*\\(?:-[0-9]+\\)?\\)" 1 
     (unless (get-text-property (match-beginning 1) 'face) 
     'lisp-dialect-fallback-face) keep)) t))
 
  (add-hook 'after-change-major-mode-hook (lambda () (font-lock-add-keywords nil
-									   
-  `(("\\(-?[0-9]+\\.?[0-9]*\\)" 0 (unless (get-text-property (match-beginning 0) 'face) 
-    'numeric-values-face) keep)
+
+  `(("\\(\\_<-?[0-9]+\\.?[0-9]*\\_>\\)" 0 'numeric-values-face keep)
 
    ("[][(){}\\+\\-/\\*=<>!&|$.;:~'?`]" 0 'punctuation-face append)))))
